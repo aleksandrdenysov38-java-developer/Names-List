200864ba8c1a153ec41c7ff0e796be2f25d1ba84
alex <aleksandrdenysov38@gmail.com>
2026-03-15 21:36:10 +0200

    Ready project

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,13 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("This is my app");
+        DataRepository repository = new DataRepository();
+        DataHandler handler = new DataHandler();
+        UIOperator uiOperator = new UIOperator();
+        uiOperator.getOutput(handler.formListOutput(repository.getData()));
+        uiOperator.getOutput(handler.formOutput(repository.getData(), 2));
     }
 }
+
